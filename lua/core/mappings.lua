08ac3a3a705886acc02a80dc10d8969faafb6898
Ben Kesby <benjamin.d.kesby@gmail.com>
2021-09-16 13:03:50 +0100

    add default mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,12 +1,27 @@
 local utils = require "core.utils"
 
---local map = utils.map
+local map = utils.map
 local cmd = vim.cmd
 
 local M = {}
 
 -- mappings to be called during initialization
 M.misc = function()
+   local function default_mappings()
+      -- don't copy the replaced text after pasting in visual mode
+      map("v", "p", '"_dP')
+
+      -- allow moving cursor through wrapped lines as default
+      map("", "j", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { expr = true })
+      map("", "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { expr = true })
+
+      -- turn off highlighting with ESC
+      map("n", "<Esc>", ":noh <CR>")
+
+      -- don't yank on cut
+      map({ "n", "v" }, "x", '"_x')
+   end
+
    local function required_mappings()
       
       -- add Packer commands because we are not loading it at startup
@@ -18,7 +33,7 @@ M.misc = function()
       cmd "silent! command PackerUpdate lua require 'plugins' require('packer').update()"
 
    end
-
+   default_mappings()
    required_mappings()
 end
 
