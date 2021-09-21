d657c91a5d039dcff6d76e13c3f0c56555e73c87
Benjamin Kesby <benjamin.d.kesby@gmail.com>
2021-09-21 16:58:52 +0100

    formatting

--- lua/plugins/packerinit.lua
+++ lua/plugins/packerinit.lua
@@ -6,27 +6,29 @@ local present, packer = pcall(require, "packer")
 
 -- install packer on new system
 if not present then
-   local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
+   local install_path = vim.fn.stdpath("data") ..
+                            "/site/pack/packer/opt/packer.nvim"
 
    print "Cloning packer..."
    -- remove the dir before cloning
    vim.fn.delete(install_path, "rf")
    vim.fn.system({
-         "git",
-         "clone",
-         "https://github.com/wbthomason/packer.nvim",
-         "--depth",
-         "1",
-         install_path,
+      "git",
+      "clone",
+      "https://github.com/wbthomason/packer.nvim",
+      "--depth",
+      "1",
+      install_path,
    })
-   
+
    cmd "packadd packer.nvim"
    present, packer = pcall(require, "packer")
 
    if present then
       print "Packer cloned successfully."
    else
-      error("Couldn't clone packer!\nPacker path: " .. packer_path .. "\n" .. packer)
+      error("Couldn't clone packer!\nPacker path: " .. packer_path .. "\n" ..
+                packer)
    end
 end
 
@@ -34,7 +36,9 @@ end
 packer.init {
    display = {
       open_fn = function()
-         return require("packer.util").float { border = "single" }
+         return require("packer.util").float {
+            border = "single",
+         }
       end,
       prompt_border = "single",
    },
