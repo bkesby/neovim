b92b373e3ba6a7aed8f87851108435f990ee248d
Benjamin Kesby <benjamin.d.kesby@gmail.com>
2021-09-24 15:03:21 +0100

    fix neoformatter for lua

--- lua/plugins/configs/neoformat.lua
+++ lua/plugins/configs/neoformat.lua
@@ -8,20 +8,21 @@ local rc = require("rc").options.plugin
 g.neoformat_lua_luaformatter = {
    exe = "lua-format",
    args = {
-      "--tab-width=3",
-      "--no-use-tab",
-      "--indent-width=3",
-      "--break-after-table-lb",
-      "--break-before-table-rb",
-      "--no-chop-down-table",
-      "--single-quote-to-double-quote",
+      "--tab-width=3", "--no-use-tab", "--indent-width=3",
+      "--break-after-table-lb", "--break-before-table-rb",
+      "--no-chop-down-table", "--single-quote-to-double-quote",
       "--no-keep-simple-control-block-one-line",
-      "--no-keep-simple-function-one-line",
-      "--extra-sep-at-table-end",
-      "--spaces-around-equals-in-field",
-      "--spaces-inside-table-braces",
+      "--no-keep-simple-function-one-line", "--extra-sep-at-table-end",
+      "--spaces-around-equals-in-field", "--spaces-inside-table-braces",
    },
 }
+
+g.neoformat_enabled_lua = { "luaformatter" }
+
+-- Python formatting
+-- g.neoformat_python_black {
+--    exe = "black",
+-- }
 
 -- Basic filetype formatting
 g.neoformat_basic_format_align = rc.neoformat.basic_format_align
