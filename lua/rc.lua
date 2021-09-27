058cc69fc1ea7f36142b26135f03c2f7ec5ddcca
Benjamin Kesby <benjamin.d.kesby@gmail.com>
2021-09-27 14:25:05 +0100

    changes to TODO

--- lua/rc.lua
+++ lua/rc.lua
@@ -1,9 +1,7 @@
 local M = {}
 
 -- TODO: Highlight bufferline
--- TODO: Setup htop terminal? < play around
 -- TODO: Add scratchpad/notes taking system
--- TODO: Add suggested coq keybindings
 
 -- UI {{{
 M.ui = {
