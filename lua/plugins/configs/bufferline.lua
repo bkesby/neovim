bd577ab91fc3ceb118a80e6f479da82116b91cfd
Ben Kesby <benjamin.d.kesby@gmail.com>
2021-09-27 16:59:12 +0100

    adds bufferline background highlighting

--- lua/plugins/configs/bufferline.lua
+++ lua/plugins/configs/bufferline.lua
@@ -3,11 +3,35 @@ if not present then
    return
 end
 
+local colors = require("colors").get()
+
 bufferline.setup {
    options = {
       numbers = "both",
       separator_style = "slant",
       diagnostics = "nvim_lsp",
       always_show_bufferline = false,
+   },
+   highlights = {
+      -- see :h bufferline-highlights
+      fill = {
+         guibg = colors.base01,
+      },
+      separator = {
+         guifg = colors.base01,
+      },
+      separator_visible = {
+         guifg = colors.base01,
+      },
+      separator_selected = {
+         guifg = colors.base01,
+      },
+      -- buffer_visible = {
+      --    guifg = colors.base0F,
+      --    guibg = colors.base0D,
+      -- },
+      -- buffer_selected = {
+      --    guifg = colors.base06,
+      -- },
    },
 }
