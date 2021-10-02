26aed22945b449f81662e31ff2a02c35749fe583
Ben Kesby <benjamin.d.kesby@gmail.com>
2021-10-02 12:20:58 +0100

    adds reload and print helper functions

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,15 +1,27 @@
-local core_modules = {
-   "core.options",
-   "core.autocmds",
-   "core.mappings",
-}
+local core_modules = { "core.options", "core.autocmds", "core.mappings" }
+
+-- Helpful print wrapper
+P = function(v)
+   print(vim.inspect(v))
+   return v
+end
+
+-- Helpful reload function
+if pcall(require, "plenary") then
+   RELOAD = require("plenary.reload").reload_module
+
+   R = function(name)
+      RELOAD(name)
+      return require(name)
+   end
+end
 
 for _, module in ipairs(core_modules) do
-   local ok, err = pcall(require, module)
+   local ok, err = pcall(R, module)
    if not ok then
       error("Error loading " .. module .. "\n\n" .. err)
    end
 end
 
 -- Run initialization mappings
- require("core.mappings").misc()
+require("core.mappings").misc()
