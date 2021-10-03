97d54d6d94759d9f89d5368b1aae4f5814f77da5
Benjamin Kesby <benjamin.d.kesby@gmail.com>
2021-10-03 18:31:18 +0100

    fixes lua formatting

--- lua/plugins/configs/neoformat.lua
+++ lua/plugins/configs/neoformat.lua
@@ -2,18 +2,15 @@ local g = vim.g
 local rc = require("rc").options.plugin
 
 -- For debugging
--- g.neoformat_verbose = 1
+g.neoformat_verbose = 1
 
 -- Lua formatting
 g.neoformat_lua_luaformatter = {
    exe = "lua-format",
    args = {
-      "--column_limit=150", "--tab-width=3", "--no-use-tab", "--indent-width=3",
-      "--break-after-table-lb", "--break-before-table-rb",
-      "--no-chop-down-table", "--single-quote-to-double-quote",
-      "--keep-simple-control-block-one-line", "--keep-simple-function-one-line",
-      "--extra-sep-at-table-end", "--spaces-around-equals-in-field",
-      "--no-break-after-operator", "--spaces-inside-table-braces",
+      "--column-limit=150", "--tab-width=3", "--no-use-tab", "--indent-width=3", "--break-after-table-lb", "--break-before-table-rb",
+      "--no-chop-down-table", "--single-quote-to-double-quote", "--keep-simple-control-block-one-line", "--keep-simple-function-one-line",
+      "--extra-sep-at-table-end", "--spaces-around-equals-in-field", "--no-break-after-operator", "--spaces-inside-table-braces",
    },
 }
 g.neoformat_enabled_lua = { "luaformatter" }
@@ -31,7 +28,7 @@ g.neoformat_rust_rustfmt = {
    exe = "rustfmt",
    replace = true,
 }
-g.neoformat_rust_enable = { "rustfmt" }
+g.neoformat_enabled_rust = { "rustfmt" }
 
 -- Basic filetype formatting
 g.neoformat_basic_format_align = rc.neoformat.basic_format_align
