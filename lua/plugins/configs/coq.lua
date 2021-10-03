1d1f03c28327e25e20124606780b669b31c1f6a7
Benjamin Kesby <benjamin.d.kesby@gmail.com>
2021-10-03 22:05:54 +0100

    attempt to debug coq (stash)

--- lua/plugins/configs/coq.lua
+++ lua/plugins/configs/coq.lua
@@ -4,6 +4,9 @@ M.setup = function()
    vim.g.coq_settings = {
       auto_start = "shut-up",
       ["clients.snippets.warn"] = {},
+      keymap = {
+         jump_to_mark = "<C-j>",
+      },
    }
 end
 
--- lua/rc.lua
+++ lua/rc.lua
@@ -287,7 +287,7 @@ M.mappings.plugin = {
 -- }}}
 -- Plugin switches {{{
 M.plugin_status = {
-   autopairs = true,
+   autopairs = false,
    bbye = true,
    better_escape = true,
    blankline = true,
