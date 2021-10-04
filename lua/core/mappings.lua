e710475910c90a7ca1d42f7e991d548b87a1b171
Benjamin Kesby <benjamin.d.kesby@gmail.com>
2021-10-05 00:27:41 +0100

    fix telescope project mapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -201,7 +201,7 @@ M.telescope = function()
    local m = plugin_maps.telescope
    map("n", m.buffers, ":Telescope buffers <CR>")
    map("n", m.find_files, ":Telescope find_files <CR>")
-   map("n", m.find_projects, ":Telescope projects<CR>")
+   map("n", m.find_projects, ":Telescope project<CR>")
    map("n", m.file_browser, ":Telescope file_browser <CR>")
    map("n", m.git_commits, ":Telescope git_commits <CR>")
    map("n", m.git_status, ":Telescope git_status <CR>")
