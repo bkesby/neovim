d87a144361a489dfbf10b9b1f29ee27e44d3ebf6
Ben Kesby <benjamin.d.kesby@gmail.com>
2021-10-06 22:35:28 +0100

    adds black format support for poetry projects

--- lua/plugins/configs/neoformat.lua
+++ lua/plugins/configs/neoformat.lua
@@ -8,9 +8,8 @@ local rc = require("rc").options.plugin
 g.neoformat_lua_luaformatter = {
    exe = "lua-format",
    args = {
-      "--column-limit=120", "--tab-width=3", "--no-use-tab", "--indent-width=3",
-      "--no-chop-down-table", "--single-quote-to-double-quote",
-      "--keep-simple-control-block-one-line", "--keep-simple-function-one-line",
+      "--column-limit=120", "--tab-width=3", "--no-use-tab", "--indent-width=3", "--no-chop-down-table",
+      "--single-quote-to-double-quote", "--keep-simple-control-block-one-line", "--keep-simple-function-one-line",
       "--extra-sep-at-table-end", "--spaces-around-equals-in-field", "--no-break-after-operator",
       "--spaces-inside-table-braces",
    }, -- "--break-after-table-lb", "--break-before-table-rb",
@@ -23,7 +22,14 @@ g.neoformat_python_black = {
    args = { "-q" },
    replace = true,
 }
-g.neoformat_enabled_python = { "black" }
+
+g.neoformat_python_poetry = {
+   exe = "poetry",
+   args = { "run", "black" },
+   replace = true,
+}
+
+g.neoformat_enabled_python = { "poetry", "black" }
 
 -- Rust formatting
 g.neoformat_rust_rustfmt = {
@@ -31,6 +37,9 @@ g.neoformat_rust_rustfmt = {
    replace = true,
 }
 g.neoformat_enabled_rust = { "rustfmt" }
+
+-- run all enabled formatters
+-- g.neoformat_run_all_formatters = 1
 
 -- Basic filetype formatting
 g.neoformat_basic_format_align = rc.neoformat.basic_format_align
