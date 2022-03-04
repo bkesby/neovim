4a6ac94e3f0130703034059d33cddd6e677b8fa1
Ben Kesby <benjamin.d.kesby@gmail.com>
2022-03-04 22:44:44 +0000

    formatting issues

--- lua/theme/init.lua
+++ lua/theme/init.lua
@@ -4,76 +4,41 @@ if not present then return end
 local rc = require("rc")
 
 theme.setup {
-   theme = "onedark",
-   options = {
-      cursorline = true,
-   },
+   theme = function()
+      if vim.o.background == "dark" then
+         return "onedark"
+      else
+         return "onelight"
+      end
+   end,
+   options = { cursorline = true },
    -- Declare highlight group additions and changes
    hlgroups = {
-      DiagnosticError = {
-         fg = "${red}",
-      },
-      DiagnosticInformation = {
-         fg = "${cyan}",
-         bg = "${bg}",
-      },
-      DiagnosticSignError = {
-         fg = "${red}",
-         bg = "${bg}",
-      },
-      DiagnosticSignWarning = {
-         fg = "${yellow}",
-         bg = "${bg}",
-      },
-      DiagnosticSignHint = {
-         fg = "${blue}",
-         bg = "${bg}",
-      },
-      DiagnosticSignInformation = {
-         fg = "${cyan}",
-         bg = "${bg}",
-      },
-      FoldColumn = {
-         link = "SignColumn",
-      },
-      GitAdd = {
-         fg = "${green}",
-      },
-      GitChange = {
-         fg = "${orange}",
-      },
-      GitDelete = {
-         fg = "${red}",
-      },
-      Identifier = {
-         fg = "${red}",
-      },
-      IndentBlanklineContextChar = {
-         fg = "${gray}",
-      },
-      LspDiagnosticsVirtualTextError = {
-         link = "LspDiagnosticsSignError",
-      },
-      LspDiagnosticsVirtualTextWarning = {
-         link = "LspDiagnosticsSignWarning",
-      },
+      DiagnosticError = { fg = "${red}" },
+      -- DiagnosticInformation = { fg = "${cyan}", bg = "${bg}" },
+      DiagnosticSignError = { fg = "${red}", bg = "${bg}" },
+      DiagnosticSignWarning = { fg = "${yellow}", bg = "${bg}" },
+      DiagnosticSignHint = { fg = "${blue}", bg = "${bg}" },
+      DiagnosticSignInformation = { fg = "${cyan}", bg = "${bg}" },
+      FoldColumn = { link = "SignColumn" },
+      GitAdd = { fg = "${green}" },
+      GitAddStatus = { fg = "${green}", bg = "${bg_statusline}" },
+      GitChange = { fg = "${orange}" },
+      GitDelete = { fg = "${red}" },
+      Identifier = { fg = "${red}" },
+      IndentBlanklineContextChar = { fg = "${gray}" },
+      LspDiagnosticsVirtualTextError = { link = "LspDiagnosticsSignError" },
+      LspDiagnosticsVirtualTextWarning = { link = "LspDiagnosticsSignWarning" },
       LspDiagnosticsVirtualTextInformation = {
          link = "LspDiagnosticsSignInformation",
       },
-      LspDiagnosticsVirtualTextHint = {
-         link = "LspDiagnosticsSignHint",
-      },
+      LspDiagnosticsVirtualTextHint = { link = "LspDiagnosticsSignHint" },
       StatusLineNC = {
          fg = "${indentline}",
          bg = "${bg}",
          style = "strikethrough",
       },
-      -- TSField = {
-      --    fg = "${highlight}",
-      -- },
-      TSFuncMacro = {
-         fg = "${red}",
-      },
+      TSFuncMacro = { fg = "${red}" },
    },
 }
 
