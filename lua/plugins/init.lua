d4904da7625616183f22244c868584e2203358c2
Ben Kesby <benjamin.d.kesby@gmail.com>
2022-04-23 15:30:30 +0100

    remove unused plugins and remove rc requirement

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,12 +2,7 @@ local present, packer = pcall(require, "plugins.packerinit")
 
 if not present then return false end
 
--- stop linting errors
-local use = packer.use
-
-return packer.startup(function()
-   local plugin_status = require("rc").plugin_status
-
+return packer.startup(function(use)
    -- plugin manager
    use { "wbthomason/packer.nvim", event = "VimEnter" }
 
@@ -18,33 +13,20 @@ return packer.startup(function()
    -- core (the pope)
    use { "tpope/vim-abolish" } -- abbreviations, substitusion and coercion
    use { "tpope/vim-repeat" } -- adds repeats for plugins
-   -- TODO: Add config for targets to add <> to b
-   use { "wellle/targets.vim" }
-   use { -- bracket mappings
-      "tpope/vim-unimpaired",
-      event = "UIEnter",
-   }
    use { "tpope/vim-sleuth" } -- shift/tab width detection
+   use { "tpope/vim-unimpaired", event = "UIEnter" } -- bracket mappings
    use {
       "machakann/vim-sandwich",
-      disable = not plugin_status.sandwich,
       setup = function() require("plugins.configs.others").sandwich() end,
       config = function() require("core.mappings").sandwich() end,
    } -- surround stuff with motion
    use {
-      "svermeulen/vim-subversive",
-      disable = not plugin_status.subversive,
-      setup = function() require("core.mappings").subversive() end,
-   }
-   use {
       "ThePrimeagen/harpoon",
-      disable = not plugin_status.harpoon,
       setup = function() require("core.mappings").harpoon() end,
       config = function() require("plugins.configs.others").harpoon() end,
    }
    use {
       "dstein64/nvim-scrollview",
-      disable = not plugin_status.scrollview,
       setup = function() require("plugins.configs.others").scrollview() end,
    }
    use {
@@ -54,6 +36,7 @@ return packer.startup(function()
    }
 
    -- text objects
+   use { "wellle/targets.vim" } -- additional text objects
    use { "kana/vim-textobj-user" }
    use { "glts/vim-textobj-comment", event = "UIEnter" }
    use { "michaeljsmith/vim-indent-object" }
@@ -61,7 +44,6 @@ return packer.startup(function()
    use { "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter" }
    use {
       "chaoren/vim-wordmotion",
-      disable = not plugin_status.wordmotion,
       event = "UIEnter",
       setup = function() require("plugins.configs.others").wordmotion() end,
    }
@@ -78,7 +60,6 @@ return packer.startup(function()
    use {
       "famiu/feline.nvim",
       as = "statusline",
-      disable = not plugin_status.statusline,
       after = "theme",
       requires = { "kyazdani42/nvim-web-devicons", opt = true },
       config = function() require("plugins.configs.statusline") end,
@@ -86,17 +67,14 @@ return packer.startup(function()
 
    use {
       "akinsho/bufferline.nvim",
-      disable = not plugin_status.bufferline,
       after = "statusline",
       config = function() require("plugins.configs.bufferline") end,
       setup = function() require("core.mappings").bufferline() end,
    }
 
    -- dashboard
-
    use {
       "glepnir/dashboard-nvim",
-      disable = not plugin_status.dashboard,
       config = function() require("plugins.configs.dashboard") end,
       setup = function() require("core.mappings").dashboard() end,
    }
@@ -104,7 +82,6 @@ return packer.startup(function()
    -- treesitter
    use {
       "nvim-treesitter/nvim-treesitter",
-      disable = not plugin_status.treesitter,
       event = "BufRead",
       config = function() require("plugins.configs.treesitter") end,
       run = ":TSUpdate",
@@ -121,15 +98,11 @@ return packer.startup(function()
       setup = function() require("plugins.configs.coq") end,
       requires = { "windwp/nvim-autopairs" },
    }
-
-   -- snippets
    use { "ms-jpq/coq.artifacts", as = "artifacts", branch = "artifacts", event = "BufRead" }
 
    -- lsp
    use { "williamboman/nvim-lsp-installer", after = "coq_nvim" }
-
    use { "RishabhRD/nvim-lsputils", after = "nvim-lsp-installer", requires = { "RishabhRD/popfix" } }
-
    use { "neovim/nvim-lspconfig", after = "nvim-lsputils", config = function() require("plugins.configs.lspconfig") end }
 
    use {
@@ -139,42 +112,35 @@ return packer.startup(function()
    }
 
    -- formatting
-
    use {
       "sbdchd/neoformat",
-      disable = not plugin_status.neoformat,
       cmd = "Neoformat",
       setup = function() require("core.mappings").neoformat() end,
       config = function() require("plugins.configs.neoformat") end,
    }
 
    -- misc
-
    use {
       "ethanholz/nvim-lastplace",
-      disable = not plugin_status.lastplace,
       config = function() require("plugins.configs.others").lastplace() end,
    }
 
-   use { "windwp/nvim-ts-autotag", disable = not plugin_status.autotag, event = "BufRead" }
+   use { "windwp/nvim-ts-autotag", event = "BufRead" }
 
    use {
       "lukas-reineke/indent-blankline.nvim",
-      disable = not plugin_status.blankline,
       event = "BufRead",
       config = function() require("plugins.configs.others").blankline() end,
    }
 
    use {
       "max397574/better-escape.nvim",
-      disable = not plugin_status.better_escape,
       event = "InsertEnter",
       config = function() require("plugins.configs.others").better_escape() end,
    }
 
    use {
       "moll/vim-bbye",
-      disable = not plugin_status.bbye,
       opt = true,
       cmd = { "Bdelete", "Bwipeout" },
       setup = function() require("core.mappings").bbye() end,
@@ -205,13 +171,11 @@ return packer.startup(function()
 
    use {
       "edluffy/specs.nvim",
-      disable = not plugin_status.specs,
       config = function() require("plugins.configs.others").specs() end,
    }
 
    use {
       "folke/todo-comments.nvim",
-      disable = not plugin_status.todo,
       event = "BufRead",
       setup = function() require("core.mappings").todo() end,
       config = function() require("plugins.configs.todo") end,
@@ -232,7 +196,6 @@ return packer.startup(function()
 
    use {
       "Pocco81/TrueZen.nvim",
-      disable = not plugin_status.zen,
       cmd = { "TZAtaraxis", "TZMinimalist", "TZFocus" },
       config = function() require("plugins.configs.zen") end,
       setup = function() require("core.mappings").zen() end,
@@ -248,7 +211,6 @@ return packer.startup(function()
       requires = {
          {
             "sudormrfbin/cheatsheet.nvim",
-            disable = not plugin_status.cheatsheet,
             after = "telescope.nvim",
             config = function() require "plugins.configs.cheatsheet" end,
             setup = function() require("core.mappings").cheatsheet() end,
@@ -261,16 +223,14 @@ return packer.startup(function()
    -- git
    use {
       "tpope/vim-fugitive",
-      disable = not plugin_status.fugitive,
       cmd = { "Git", "Gdiffsplit", "Gsplit", "Gread", "Gedit" },
       setup = function() require("core.mappings").fugitive() end,
    }
 
-   use { "tpope/vim-rhubarb", disable = not plugin_status.fugitive, after = "vim-fugitive" }
+   use { "tpope/vim-rhubarb", after = "vim-fugitive" }
 
    use {
       "lewis6991/gitsigns.nvim",
-      disable = not plugin_status.gitsigns,
       opt = true,
       config = function() require("plugins.configs.gitsigns") end,
       setup = function() require("core.utils").lazy_load("gitsigns.nvim") end,
@@ -279,8 +239,6 @@ return packer.startup(function()
    -- debugging
    use {
       "mfussenegger/nvim-dap",
-      disable = not plugin_status.dap,
-      -- TODO: Work out lazyload trigger -- only on breakpoint set or continue()
       setup = function() require("core.mappings").dap() end,
       config = function() require("plugins.configs.dap") end,
       requires = { "mfussenegger/nvim-dap-python" },
@@ -294,9 +252,7 @@ return packer.startup(function()
 
    use {
       "simrat39/rust-tools.nvim",
-      disable = not plugin_status.rust_tools,
       after = "nvim-lspconfig",
-      -- ft = "rust",
       config = function() require("plugins.configs.rust_tools") end,
    }
 
@@ -321,29 +277,6 @@ return packer.startup(function()
          require("plugins.configs.others").markdown()
          require("core.mappings").markdown()
       end,
-   }
-   use {
-      "ellisonleao/glow.nvim",
-      disable = not plugin_status.glow,
-      cmd = "Glow",
-      setup = function() require("core.mappings").glow() end,
-   }
-
-   -- scratchpad
-   use {
-      "metakirby5/codi.vim",
-      disable = not plugin_status.codi,
-      cmd = "Codi",
-      config = function() require("plugins.configs.others").codi() end,
-   }
-
-   -- file manager
-   use {
-      "ms-jpq/chadtree",
-      disable = not plugin_status.chadtree,
-      branch = "chad",
-      cmd = "CHADopen",
-      run = ":silent! CHADdeps",
    }
 
 end)
