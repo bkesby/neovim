3f9505578184ee804081cb04b126339db883443a
Ben Kesby <benjamin.d.kesby@gmail.com>
2022-04-23 15:09:41 +0100

    move packer init to be loaded from plugins/init.lua

--- lua/plugins/packerinit.lua
+++ lua/plugins/packerinit.lua
@@ -1,29 +1,33 @@
+-- Automatically install packer
+local fn = vim.fn
 local cmd = vim.cmd
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 cmd "packadd packer.nvim"
 
 local present, packer = pcall(require, "packer")
 
--- install packer on new system
+-- bootstrap packer on fresh install
 if not present then
-   local packer_path = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
-
-   print "Cloning packer..."
-   -- remove the dir before cloning
-   vim.fn.delete(packer_path, "rf")
-   vim.fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", "--depth", "1", packer_path })
-
-   cmd "packadd packer.nvim"
-   present, packer = pcall(require, "packer")
-
-   if present then
-      print "Packer cloned successfully."
-   else
-      error("Couldn't clone packer!\nPacker path: " .. packer_path .. "\n" .. packer)
-   end
+   packer_bootstrap = fn.system({
+      'git',
+      'clone',
+      '--depth',
+      '1',
+      'https://github.com/wbthomason/packer.nvim',
+      install_path
+   })
 end
 
--- initialize packer with custom settings
+-- Autocommand to reload neovim on plugin init file save
+cmd [[
+   augroup packer_user_config
+      autocmd!
+      autocmd BufWritePost init.lua source <afile> | PackerCompile
+   augroup end
+]]
+
+-- Initialize packer with custom settings
 packer.init {
    display = {
       open_fn = function()
