882c4711bbbef3b0492c2a3f74713e434cd17cfa
Ben Kesby <benjamin.d.kesby@gmail.com>
2022-04-23 13:07:43 +0100

    remove failing reload function

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,13 +1,8 @@
 local core_modules = { "options", "autocmds", "mappings", "globals" }
 
 for _, module in ipairs(core_modules) do
-   if pcall(require, "plenary") then
-      local ok, err = pcall(R, "core." .. module)
-      if not ok then error("Error loading " .. module .. "\n\n" .. err) end
-   else
-      local ok, err = pcall(require, "core." .. module)
-      if not ok then error("Error loading " .. module .. "\n\n" .. err) end
-   end
+   local ok, err = pcall(require, "core." .. module)
+   if not ok then error("Error loading " .. module .. "\n\n" .. err) end
 end
 
 -- Run initialization mappings
