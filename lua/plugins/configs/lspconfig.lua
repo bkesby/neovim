884205e964e9e2f9259f0c6c11bde1473f701f47
Ben Kesby <benjamin.d.kesby@gmail.com>
2022-05-05 17:11:31 +0100

    fix: lsp initial setup

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -4,12 +4,19 @@ local present3, coq = pcall(require, "coq")
 
 if not (present1 or present2 or present3) then return end
 
-local lsp = require('maps').plugin.lsp
+local maps = require('maps').plugin.lsp
+local set_keymap = vim.api.nvim_set_keymap
+
+local opts = { noremap=true, silent=true }
+
+set_keymap('n', maps.open_float, '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+set_keymap('n', maps.goto_prev, '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+set_keymap('n', maps.goto_next, '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+set_keymap('n', maps.set_loclist, '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
 -- use attach function to only map keys after language server attaches to current buffer
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-   -- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
    local opts = {
       noremap = true,
@@ -17,24 +24,20 @@ local on_attach = function(_, bufnr)
    }
 
    -- see `:help vim.lsp.*` for documentation on any of the below functions
-   buf_set_keymap("n", lsp.declaration, "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-   buf_set_keymap("n", lsp.definition, "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-   buf_set_keymap("n", lsp.hover, "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-   buf_set_keymap("n", lsp.implementation, "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-   buf_set_keymap("n", lsp.signature_help, "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-   buf_set_keymap("n", lsp.add_workspace_folder, "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-   buf_set_keymap("n", lsp.remove_workspace_folder, "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-   buf_set_keymap("n", lsp.list_workspace_folders,
+   buf_set_keymap("n", maps.declaration, "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+   buf_set_keymap("n", maps.definition, "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+   buf_set_keymap("n", maps.hover, "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+   buf_set_keymap("n", maps.implementation, "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+   buf_set_keymap("n", maps.signature_help, "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+   buf_set_keymap("n", maps.add_workspace_folder, "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+   buf_set_keymap("n", maps.remove_workspace_folder, "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+   buf_set_keymap("n", maps.list_workspace_folders,
                   "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-   buf_set_keymap("n", lsp.type_definition, "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-   buf_set_keymap("n", lsp.rename, "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-   buf_set_keymap("n", lsp.code_action, "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-   buf_set_keymap("n", lsp.references, "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-   buf_set_keymap("n", lsp.show_line_diagnostics, "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-   buf_set_keymap("n", lsp.goto_next, "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-   buf_set_keymap("n", lsp.goto_prev, "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-   buf_set_keymap("n", lsp.set_loclist, "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-   buf_set_keymap("n", lsp.formatting, "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+   buf_set_keymap("n", maps.type_definition, "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+   buf_set_keymap("n", maps.rename, "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+   buf_set_keymap("n", maps.code_action, "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+   buf_set_keymap("n", maps.references, "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+   buf_set_keymap("n", maps.formatting, "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
    local handlers = vim.lsp.handlers
    -- extended lsp utils
@@ -78,39 +81,35 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
    update_in_insert = false,
 })
 
+local servers = { 
+   "bashls", 
+   "cssls",
+   "dockerls",
+   "eslint",
+   "html",
+   "jsonls",
+   "tsserver",
+   "sumneko_lua",
+   "phpactor",
+   "pyright",
+   "rust_analyzer",
+   "sqls",
+   "svelte",
+   "taplo",
+   "yamlls",
+}
+
 -- nvim-lsp-installer
-lspinstall.on_server_ready(function(server)
-   local opts = {
-      on_attach = on_attach,
-      flags = {
-         debouce_text_changes = 150,
-      },
-   }
-   -- neovim configuration help
-   if server == "sumneko_lua" then
-      opts.on_new_config = function(opt, root_dir)
-         local conf_dir = root_dir == vim.fn.stdpath("config")
-         local dev_dir = vim.fn.fnamemodify(root_dir, ":h") == vim.fn.expand("~/Projects/neovim")
-         local lua_dev = require("lua-dev").setup({
-            library = {
-               vimruntime = true,
-               types = true,
-               plugins = true,
-            },
-            lspconfig = {
-               globals = { "vim" },
-            },
-         })
-         if conf_dir or dev_dir then opt.settings = lua_dev end
-      end
-   end
-   -- typescript configuration
-   if server == "tsserver" then opts.cmd = { "yarn", "typescript-language-server", "--stdio" } end
-   -- svelte configuration
-   if server == "svelte" then opts.cmd = { "yarn", "svelteserver", "--stdio" } end
+lspinstall.setup {
+   ensure_installed = servers,
+   automatic_installation = true,
+}
 
-   server:setup(coq.lsp_ensure_capabilities(opts))
-   vim.cmd [[ do User LspAttachBuffers ]]
-end)
-
--- LspUtils options
+-- LSP config server setup
+for _, server in pairs(servers) do
+   local opts = { on_attach = on_attach }
+   -- Server specific options
+   
+   lspconfig[server].setup(opts)
+   lspconfig[server].setup(coq.lsp_ensure_capabilities(opts))
+end
